dd794be1cb2e6589f21ab0562c3b2e5aff041432
mborejdo <mborejdo+github@gmail.com>
2022-03-18 11:28:27 +0100

    refactor

--- data/index.ts
+++ data/index.ts
@@ -1,99 +1,18 @@
+import { gitConfigure, gitClone, gitPush } from "./git.ts";
 import { exists, move } from "https://deno.land/std@0.129.0/fs/mod.ts";
-import { join } from "https://deno.land/std@0.129.0/path/mod.ts";
 
 const { run, writeTextFile, env } = Deno;
-const API_TOKEN_GITHUB = env.get("API_TOKEN_GITHUB");
-const DESTFOLDER = "/github/workspace/data/mborejdo";
+const API_TOKEN_GITHUB = env.get("API_TOKEN_GITHUB") || "";
+const REPO = env.get("REPO") || "mborejdo";
+
+const BASEFOLDER = "/github/workspace/data";
+const DESTFOLDER = `${BASEFOLDER}/${REPO}`;
+
 const twitterUrl = "https://www.twitter.com/mediacoder";
 const linkedInUrl = "https://www.linkedin.com/in/michael-borejdo-a7367928/";
 const instagramUrl = "https://www.instagram.com/mediacoder/";
 const badgeHeight = "20";
 
-
-/**
- * 
- * @returns 
- */
-function gitConfigure(): Promise<boolean> {
-  return new Promise((resolve: (value: boolean) => void) => {
-    const config1 = run({
-      cmd: ["git", "config", "--global", "user.email", "mborejdo+github@gmail.com"],
-    });
-    config1.status().then((configResult) => {
-      if (configResult.success) {
-        const config2 = run({
-          cmd: ["git", "config", "--global", "user.name", "Michael Borejdo"],
-        });
-        config2.status().then((configResult2) => {
-          if (configResult2.success) {
-            const config3 = run({
-              cmd: ["git", "config", "--global", "user.password", API_TOKEN_GITHUB || ""],
-            });
-            config3.status().then((configResult3) => {
-              resolve(configResult3.success);
-            });
-          } else {
-            resolve(false);
-          }
-        });
-      } else {
-        resolve(false);
-      }
-    });
-  });
-}
-
-/**
- * 
- * @param source 
- * @param dest 
- * @returns 
- */
-function gitClone(source: string, dest: string): Promise<boolean> {
-  return new Promise(async (resolve: (value: boolean) => void) => {
-    const isCloned = await exists(join(dest, ".git"));
-
-    if (isCloned) {
-      console.log(`Repo '${source}' already exists`);
-      resolve(false)
-      return;
-    }
-
-    console.log(`Cloning '${source}' into '${dest}'...`);
-
-    const clone = run({
-      cmd: ["git", "clone", source, dest],
-    });
-    const cloneResult = await clone.status();
-
-    resolve(cloneResult.success)
-  });
-}
-
-/**
- * 
- * @returns 
- */
-function gitPush() {
-  return new Promise(async (resolve: (value: boolean) => void) => {
-    const add = run({
-      cmd: ["git", "-C", DESTFOLDER, "add", "."],
-    });
-    const addResult = await add.status();
-    const commit = run({
-      cmd: ["git", "-C", DESTFOLDER, "commit", "-a", "-m", "Update Repo"],
-    });
-    const commitResult = await commit.status();
-    console.log("PUSHING", commitResult)
-    if (commitResult.success) {
-      const commit = run({
-        cmd: ["git", "-C", DESTFOLDER, "push", "--force"],
-      });
-      const commitResult = await commit.status();
-    }
-    resolve(true)
-  });
-}
 
 /**
  * 
@@ -116,7 +35,9 @@ function patchReadme() {
 }
 
 
-await gitConfigure();
+await gitConfigure(API_TOKEN_GITHUB);
 await gitClone(`https://${API_TOKEN_GITHUB}@github.com/mborejdo/mborejdo.git`, DESTFOLDER);
+
 await patchReadme();
-await gitPush();
+
+await gitPush(DESTFOLDER);
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run -e API_TOKEN_GITHUB=${API_TOKEN_GITHUB}  --rm -v $(pwd)/data:/github/workspace/data/ --workdir /github/workspace/data/ mborejdo/action-rusty "deno run -A --quiet ./index.ts"
+docker run -e REPO="mborejdo" -e API_TOKEN_GITHUB=${API_TOKEN_GITHUB}  --rm -v $(pwd)/data:/github/workspace/data/ --workdir /github/workspace/data/ mborejdo/action-rusty "deno run -A --quiet ./index.ts"
